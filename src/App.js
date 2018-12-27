bfdd5dce9d17417e9fe7e63d6b9eb832c16c5438
Nick <plus0bit@gmail.com>
2018-12-27 23:21:55 +0300

    Set the states and create validation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const API_KEY = "8c6043515dfeeec0df742824053f70f8";
 
 class App extends React.Component {
 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -15,7 +24,28 @@ class App extends React.Component {
 
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
-    console.log(data);
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value."
+
+      });
+    }
   }
 
   render() {
@@ -23,7 +53,16 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+
+
+        />
       </div>
     );
   }
